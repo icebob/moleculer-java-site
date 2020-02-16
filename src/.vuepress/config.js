e8d23cb03c5c4ab319d67e1d334c3346ca8f431c
András Berkes <andras.berkes@mail.com>
2020-02-16 14:09:50 +0100

    Robots

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -23,7 +23,28 @@ module.exports = {
 					scrollOffset: 0
 				}
 			}
-		]        
+		],
+        [
+            'robots',
+            {
+                host: "https://moleculer-java.github.io/site",
+                disallowAll: false,
+                allowAll: true,
+                sitemap: "/sitemap.xml",
+                policies: [
+                    {
+                        userAgent: '*'
+                    }
+                ]
+            }
+        ],
+        [
+            'sitemap',
+            {
+                hostname: 'https://moleculer-java.github.io/site',
+                exclude: ['/404.html']
+            }
+        ]
     ],
 	head: [
 		['link', { rel: 'icon', href: 'favicon.ico'}]
